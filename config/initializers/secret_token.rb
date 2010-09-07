86b2164bcff04c95e1475d00cc70fc4cccb266b9
Lev <lev.panov@gmail.com>
2010-09-07 23:06:01 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mfitter::Application.config.secret_token = '160ffc931b72aac6aa00bdec0451965d39b634934c110794dd27ddf989331cdab33c4a9eb49a2cc5fff2f7b6f49d0a500f528550552ef52b7e2857c0e2e7a79c'
